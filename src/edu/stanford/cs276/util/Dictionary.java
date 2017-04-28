a6cec5513c73a559a42b0abe44a4f6daffe4b1d4
Pratyaksh Sharma <pratyaksh@me.com>
2017-04-27 20:21:51 -0700

    removed probability computation from LanguageModel

--- src/edu/stanford/cs276/util/Dictionary.java
+++ src/edu/stanford/cs276/util/Dictionary.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class Dictionary implements Serializable {
 
   private int termCount;
-  private HashMap<String, Pair<Integer, Double>> map;
+  private HashMap<String, Integer> map;
 
   public int termCount() {
     return termCount;
@@ -16,40 +16,28 @@ public class Dictionary implements Serializable {
 
   public Dictionary() {
     termCount = 0;
-    map = new HashMap<String, Pair<Integer, Double>>();
+    map = new HashMap<String, Integer>();
   }
 
   public void add(String term) {
     termCount++;
     if (map.containsKey(term)) {
-      map.get(term).setFirst(map.get(term).getFirst() + 1);
-//      map.put(term, new Pair<Integer, Double>(map.get(term).getFirst() + 1, 0.0));
+      map.put(term, map.get(term) + 1);
     } else {
-      map.put(term, new Pair<Integer, Double>(1, 0.0));
+      map.put(term, 1);
     }
   }
 
   public int count(String term) {
     if (map.containsKey(term)) {
-      return map.get(term).getFirst();
+      return map.get(term);
     } else {
       return 0;
     }
   }
 
-  public void updateProb(String term, Double prob) {
-      map.get(term).setSecond(prob);
-  }
 
-  public Double getProb(String term) {
-      if (map.containsKey(term)) {
-          return map.get(term).getSecond();
-      } else {
-          return 0.;
-      }
-  }
-
-  public Iterator<Map.Entry<String, Pair<Integer, Double>>> getIterator() {
+  public Iterator<Map.Entry<String, Integer>> getIterator() {
       return map.entrySet().iterator();
   }
 }
