40e3b6310495681cf6ab882a8173c649745b46b0
Daniel Weichieh Hsu <dwhsu@corn28.stanford.edu>
2017-04-30 16:23:49 -0700

    updated empirical, still need to write editProb function

--- src/edu/stanford/cs276/EmpiricalCostModel.java
+++ src/edu/stanford/cs276/EmpiricalCostModel.java
@@ -3,6 +3,7 @@ package edu.stanford.cs276;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -12,14 +13,13 @@ import java.util.Scanner;
 public class EmpiricalCostModel implements EditCostModel {
 	private static final long serialVersionUID = 1L;
 	
-	//way too big? todo - change to hashmap or sparse
-	int[][] del = new int[256][256];
-	int[][] ins = new int[256][256];
-	int[][] sub = new int[256][256];
-	int[][] trans = new int[256][256];
-	
-	int[] char_count = new int[256];
-	int[][] bigram_count = new int[256][256];
+	HashMap<String, Integer> del_count = new HashMap<String, Integer>();		//del_x_y - 	xy typed as x
+	HashMap<String, Integer> ins_count = new HashMap<String, Integer>();		//ins_x_y - 	x typed as xy
+	HashMap<String, Integer> sub_count = new HashMap<String, Integer>();		//sub_x_y - 	y typed as x
+	HashMap<String, Integer> trans_count = new HashMap<String, Integer>();		//trans_x_y - 	xy typed as yx
+	HashMap<String, Integer> unigram_count = new HashMap<String, Integer>();
+	HashMap<String, Integer> bigram_count = new HashMap<String, Integer>();
+
 	
   public EmpiricalCostModel(String editsFile) throws IOException {
     BufferedReader input = new BufferedReader(new FileReader(editsFile));
@@ -34,24 +34,110 @@ public class EmpiricalCostModel implements EditCostModel {
        * TODO: Your code here
        */
       
-      //build_confusion_matrix(noisy, clean);
-    }
+      //Count unigrams and bigrams
+      char prev = '#';
+      for(char c : clean.toCharArray()) {
+    	  incr_unigram_count(c);
+    	  incr_bigram_count(prev + String.valueOf(c));
+    	  prev = c;
+      }      
+      incr_bigram_count(String.valueOf(prev) + '#');
+      
+      //Build confusion matrix
+      String edit_type = identify_edit(noisy, clean); //<type>$x$y
 
+      if(edit_type.startsWith("del")) {
+    	  incr_confusion_matrix(del_count, edit_type);
+      } else if (edit_type.startsWith("ins")) {
+    	  incr_confusion_matrix(ins_count, edit_type);
+      } else if (edit_type.startsWith("sub")) {
+    	  incr_confusion_matrix(sub_count, edit_type);
+      } else if (edit_type.startsWith("trans")) {
+    	  incr_confusion_matrix(trans_count, edit_type);
+      } else {
+    	  System.out.println("unidentified edit type");
+      }
+
+    }
+    
     input.close();
     System.out.println("Done.");
   }
 
-  private void build_confusion_matrix(String noisy, String clean){
+  private void incr_confusion_matrix(HashMap<String, Integer> edit_count_matrix, String edit_key) {
+	  if(edit_count_matrix.containsKey(edit_key))
+		  edit_count_matrix.put(edit_key, edit_count_matrix.get(edit_key) + 1);
+	  else
+		  edit_count_matrix.put(edit_key, 1);
+  }
+    
+  private void incr_unigram_count(char c) {
+	  String unigram = String.valueOf(c);
 	  
-	  //inefficent, will fix later
-	  char_count[clean.charAt(0)]++;
-	  for(int i = 1; i < clean.length(); i++) {
-		  char_count[clean.charAt(i)]++;
-		  bigram_count[clean.charAt(i - 1)][clean.charAt(i)]++;
-	  }
+	  if(unigram_count.containsKey(unigram))
+		  unigram_count.put(unigram, unigram_count.get(unigram) + 1);
+	  else
+		  unigram_count.put(unigram, 1);
+  }
+  
+  private void incr_bigram_count(String bigram) {
+	  if(bigram_count.containsKey(bigram))
+		  bigram_count.put(bigram, bigram_count.get(bigram) + 1);
+	  else
+		  bigram_count.put(bigram, 1);
+  }
+  
+  private String identify_edit(String noisy, String clean) {
+	  String edit_type = "";
+	  char x = 0;
+	  char y = 0;
 	  
-	  if(noisy.length() == clean.length()) { //sub or trans
+	  if (noisy.length() > clean.length()) { //ins - x typed as xy
+		  edit_type = "ins";
 		  
+		  for(int i = 0; i < clean.length(); i++) {
+			  if (noisy.charAt(i) == clean.charAt(i))
+				  continue;
+			  
+			  if (i > 0) {
+				  x = noisy.charAt(i - 1);
+				  y = noisy.charAt(i);
+				  break;
+			  } else {
+				  x = '#';
+				  y = noisy.charAt(0);
+				  break;
+			  }
+		  }
+		  
+		  if (x == 0) {
+			  x = noisy.charAt(noisy.length() - 2);
+			  y = noisy.charAt(noisy.length() - 1);
+		  }
+	  } else if (noisy.length() < clean.length()) { //del - xy typed as x
+		  edit_type = "del";
+		  
+		  for(int i = 0; i < noisy.length(); i++) {
+			  if (noisy.charAt(i) == clean.charAt(i))
+				  continue;
+			  
+			  if (i > 0) {
+				  x = clean.charAt(i - 1);
+				  y = clean.charAt(i);
+				  break;
+			  }
+			  else {
+				  x = '#';
+				  y = clean.charAt(0);
+				  break;
+			  }
+		  }
+
+		  if(x == 0) {
+			  x = clean.charAt(clean.length() - 2);
+			  y = clean.charAt(clean.length() - 1);
+		  }
+	  } else { //sub or trans
 		  for(int i = 0; i < noisy.length(); i++) {
 			  char noisy_c = noisy.charAt(i);
 			  char clean_c = clean.charAt(i);
@@ -59,54 +145,26 @@ public class EmpiricalCostModel implements EditCostModel {
 			  if(noisy_c == clean_c)
 				  continue;
 			  
-			  if(i == noisy.length() - 1 || noisy.charAt(i + 1) == clean.charAt(i + 1)) {
-				  //sub
-				  sub[noisy_c][clean_c]++;
+			  if(i == noisy.length() - 1 || noisy.charAt(i + 1) == clean.charAt(i + 1)) { //sub - y typed as x
+				  edit_type = "sub";
+				  x = noisy_c;
+				  y = clean_c;
 				  break;
-			  } else {
-				  //trans
-				  trans[clean_c][clean.charAt(i + 1)]++;
-				  break;
-			  }
-		  }		  
-	  } else if (noisy.length() > clean.length()){ //ins
-		  for(int i = 0; i < clean.length(); i++) {
-			  if (noisy.charAt(i) == clean.charAt(i))
-				  continue;
-			  
-			  if (i > 0) {
-				  ins[noisy.charAt(i - 1)][noisy.charAt(i)]++;
-				  break;
-			  } else {
-				  ins[0][noisy.charAt(0)]++;
+			  } else { //trans - xy typed as yx
+				  edit_type = "trans";
+				  x = clean_c;
+				  y = clean.charAt(i + 1);
 				  break;
 			  }
 		  }
-		  
-		  ins[noisy.charAt(noisy.length() - 2)][noisy.charAt(noisy.length() - 1)]++;
-	  } else { //del
-		  for(int i = 0; i < noisy.length(); i++) {
-			  if (noisy.charAt(i) == clean.charAt(i))
-				  continue;
-			  
-			  if (i > 0) {
-				  del[clean.charAt(i - 1)][clean.charAt(i)]++;
-				  return;
-			  }
-			  else {
-				  del[0][clean.charAt(0)]++;
-				  return;
-			  }
-		  }
-		  
-		  del[clean.charAt(clean.length() - 2)][clean.charAt(clean.length() - 1)]++;
 	  }
+	  
+	  return edit_type + "$" + x + "$" + y;
   }
   
   // You need to add code for this interface method to calculate the proper empirical cost.
   @Override
   public double editProbability(String original, String R, int distance) {
-    //return 0.5;
     /*
      * TODO: Your code here
      */
